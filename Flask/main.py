64389db85bcb116f37997a0da0ced11dde867a07
Alejandro Moya <alejandro144ago@gmail.com>
2023-02-11 00:35:24 -0500

    Main Flask

--- Flask/main.py
+++ Flask/main.py
@@ -1,12 +1,13 @@
 from flask import Flask
 from flask import render_template
 
-app = Flask(__name__,template_folder='../Angular')
+PATH_FRONT = '../Angular/horizon-fe/src/'
+app = Flask(__name__,template_folder=PATH_FRONT)
 
 
 @app.route('/')
 def index():
-    return render_template('/Frontend/inicio_sesion.html')
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1',debug=False)

3eaf2f4f48c2caa77c09d15a762dd3f35b458ce7
Mateo Sarzosa Valencia <mateosarzosa@gmail.com>
2023-02-24 15:18:13 -0500

    continuar con registro plis

--- attempt2/aplicacion.py
+++ attempt2/aplicacion.py
@@ -14,7 +14,7 @@ def home():
     usuariosReceived = usuarios.find()
     return render_template('inicio.html', usuarios = usuariosReceived)
 
-
+#--------------------TODO ESTO CORRESPONDE A INDEX.HTML----------------------------
 # method POST
 @vistaPrincipal.route('/usuarios',methods=['POST'])
 def addUsuario():
@@ -57,6 +57,53 @@ def edit(usuario_name):
     else:
         return notFound()
 
+#--------------------HASTA ACÁ CORRESPONDE A INDEX.HTML----------------------------
+#--------------------TODO ESTO CORRESPONDE A REGISTRO.HTML----------------------------
+# method POST
+@vistaPrincipal.route('/registro',methods=['POST'])
+def addUsuario():
+    # coleccion o tabla donde se llena
+    usuarios = db['usuarios']
+
+    name = request.form['name']
+    price = request.form['price']
+    quantity = request.form['quantity']
+
+    if name and price and quantity:
+        usuario = Usuario(name,price,quantity)
+        usuarios.insert_one(usuario.toDBCollection())
+        response = jsonify({
+            'name': name,
+            'price': price,
+            'quantity': quantity
+        })
+        return redirect(url_for('inicio'))
+    else:
+        return notFound()
+
+# method delete
+@vistaPrincipal.route('/delete/<string:usuario_name>')
+def delete(usuario_name):
+    usuarios = db['usuarios']
+    usuarios.delete_one({'name': usuario_name})
+    return redirect(url_for('home'))
+
+# method put
+@vistaPrincipal.route('/edit/<string:usuario_name>', methods=['POST'])
+def edit(usuario_name):
+    usuarios = db['usuarios']
+    name = request.form['name']
+    price = request.form['price']
+    quantity = request.form['quantity']
+
+    if name and price and quantity:
+        usuarios.update_one({'name': usuario_name},{'$set' : {'name':name,'price':price,'quantity':quantity}})
+        response = jsonify({'message':'Usuario ' + usuario_name + 'actualizado correctamente :)'})
+        return redirect(url_for('home'))
+    else:
+        return notFound()
+
+#--------------------HASTA ACÁ CORRESPONDE A INDEX.HTML----------------------------
 
 @vistaPrincipal.errorhandler(404)
 def notFound(error=None):
@@ -68,13 +115,20 @@ def notFound(error=None):
     response.status_code=404
     return response
 
-### PAGINA
-# method GEST
+### PAGINA INICIO DE SESION
+# method GET para llamar con el enlace
 @vistaPrincipal.route('/sesion',methods=['GET'])
-def page():
+def sesion():
     # usuarios = db['usuarios']
     # usuariosReceived = usuarios.find()
     return render_template('sesion.html')
+
+### PAGINA INICIO DE SESION
+@vistaPrincipal.route('/registro',methods=['GET'])
+def registro():
+    # usuarios = db['usuarios']
+    # usuariosReceived = usuarios.find()
+    return render_template('registro.html')
 
 if __name__ == '__main__':
     vistaPrincipal.run(debug=True, port=4000)
